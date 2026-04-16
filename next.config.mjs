cc8b2697482faebe7a96db427d833b886674b80c
priyank viradiya <priyankviradiya227@gmail.com>
2026-04-17 00:14:33 +0530

    issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ import withSerwistInit from '@serwist/next';
 const withSerwist = withSerwistInit({
   swSrc: 'src/app/sw.js',
   swDest: 'public/sw.js',
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV !== 'production',
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  turbopack: {},
 };
 
 export default withSerwist(nextConfig);
